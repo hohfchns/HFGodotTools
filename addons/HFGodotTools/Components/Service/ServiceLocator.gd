364816fed351d810ef00217ffbb5fca3b0eedca5
hohfchns <hohfchns@gmail.com>
2021-11-06 19:40:56 +0200

    Initial commit

---
+++ addons/HFGodotTools/Components/Service/ServiceLocator.gd
@@ -0,0 +1,54 @@
+extends Node
+class_name ServiceLocator
+
+
+# Add to this dict as "ServiceName" : rf_service
+var __mrf_services: Dictionary = {}
+
+export(String, DIR) var __m_null_service_dir
+
+
+signal service_changed(service_name)
+signal new_service(service_name)
+
+
+func get_service(identity: Node, service_name: String) -> Node:
+	if service_name in __mrf_services:
+		return __mrf_services[service_name]
+	# The above code returns from the function, so no need for extra indentation
+	
+	var n_service_name: String = "Null%s" % service_name
+	var null_service: Resource = load("%s/%s.gd" % [__m_null_service_dir, n_service_name])
+	if null_service:
+		printerr("Warning! %s tried to retrieve a non-existant service by the name of %s, returning null service!" \
+			 % [identity.name, service_name])
+		return null_service.new()
+	else:
+		for i in range(10):
+			printerr("Warning! Null service file for %s could not be found!" % service_name)
+		
+		return null
+
+
+func provide_service(service_name: String, rf_service: Node) -> void:
+	__mrf_services[service_name] = rf_service
+	emit_signal("new_service", service_name)
+
+func provide_service_dict(dict: Dictionary) -> void:
+	for i in range(dict.size()):
+		provide_service(dict.keys()[i], dict.values()[i])
+
+
+func remove_service(service_name: String) -> void:
+	__mrf_services.erase(service_name)
+	emit_signal("service_changed", service_name)
+
+
+func overwrite_service(service_name: String, rf_new_service: Node) -> void:
+	__mrf_services[service_name] = rf_new_service
+	emit_signal("service_changed", service_name)
+
+
+func get_available_services() -> Array:
+	return __mrf_services.keys()
+
